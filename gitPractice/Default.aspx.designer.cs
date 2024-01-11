873c1dc7c3f2b6e778c8d91652eb2a00babd6a35
Ris <appalarajuk383@gmail.com>
2024-01-11 17:29:43 +0530

    contact

--- gitPractice/Default.aspx.designer.cs
+++ gitPractice/Default.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace gitPractice {
     public partial class _Default {
         
         /// <summary>
-        /// lbblLasstname control.
+        /// lbblLastname control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbblLasstname;
+        protected global::System.Web.UI.WebControls.Label lbblLastname;
         
         /// <summary>
         /// txtFirstname control.
